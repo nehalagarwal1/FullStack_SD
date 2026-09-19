c9d9620d51b8124efd2acbbde4d8eb2e7db5edf6
Nehal Agarwal <nehalagarwal@Nehals-MacBook-Air-3.local>
2026-09-19 12:22:00 +0530

    Activity 2 commited

--- java-gdb-activities-10/activity2/src/com/gdb/tests/TestAccount.java
+++ java-gdb-activities-10/activity2/src/com/gdb/tests/TestAccount.java
@@ -11,16 +11,28 @@ public class TestAccount {
         Account acc = new Account("ACC1001", "Rajesh Sharma", 28, 5000.0, "SAVINGS", "ACTIVE");
 
         // TODO: Step 1 - Test Initial Balance (Assert balance == 5000.0)
+        // System.out.println(acc.getBalance());
 
         // TODO: Step 2 - Test Valid Deposit (Deposit 2000.0 -> Assert balance == 7000.0)
+        // acc.deposit(2000);
+        // System.out.println(acc.getBalance());
 
         // TODO: Step 3 - Test Negative Deposit (Deposit -500.0 -> Assert returns false and balance stays 7000.0)
+        // System.out.println(acc.deposit(-500));
+        // System.out.println(acc.getBalance());
 
         // TODO: Step 4 - Test Valid Withdrawal (Withdraw 3000.0 -> Assert balance == 4000.0)
+        // acc.withdraw(3000);
+        // System.out.println(acc.getBalance());
 
         // TODO: Step 5 - Test Exceeding Withdrawal (Withdraw 10000.0 -> Assert returns false and balance stays 4000.0)
+        // System.out.println(acc.withdraw(10000));
+        // System.out.println(acc.getBalance());
 
         // TODO: Step 6 - Test Negative Withdrawal (Withdraw -100.0 -> Assert returns false and balance stays 4000.0)
+        // System.out.println(acc.withdraw(-100));
+        // System.out.println(acc.getBalance());
+
 
         System.out.println("=== Complete Activity 2 unit tests and verify output ===");
     }
